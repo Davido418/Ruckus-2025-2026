6a6b904135886aa8990a03629d50b4f92684e183
ryder_liou <ryder_liou@avenues-ny.org>
2025-12-02 16:41:25 -0500

    Made getTxOdo function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
@@ -3,24 +3,39 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.MAIN.AutoFirst;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 public class Odometry {
+    String Alliance = "blue";
+    double blueBasketX=5;//set
+    double blueBasketY=5;//set
+    double redBasketX=5;//set
+    double redBasketY=5;//set
 
     private MecanumDrive drive;
 
     public double getX() {
         drive.updatePoseEstimate();
-        return drive.localizer.getPose().position.x;
+        return 72-drive.localizer.getPose().position.x;
     }
 
     public double getY() {
         drive.updatePoseEstimate();
-        return drive.localizer.getPose().position.y;
+        return 72-drive.localizer.getPose().position.y;
     }
 
     public double getHeading() {
         drive.updatePoseEstimate();
         return drive.localizer.getPose().heading.toDouble();  // radians
     }
+    public double getTxOdo(){
+        if(Alliance == "red"){
+            return Math.atan((getY()-blueBasketY)/(getX()-blueBasketX));
+
+        }else{
+            return Math.atan((getY()-redBasketY)/(redBasketX-getX()));
+        }
+    }
 }
+
